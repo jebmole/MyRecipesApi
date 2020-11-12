2799725a2d901bdc42f4783dca9081ca71eac3fc
Jonathan Estrada Betancur <jestrada@growthaccelerationpartners.com>
2020-11-11 19:15:13 -0500

    Add project files.

---
+++ MyRecipes.Apis/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyRecipes.Apis.Models
+{
+    public partial class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ MyRecipes.Apis/Models/MyRecipesContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyRecipes.Apis.Models
+{
+    public partial class MyRecipesContext : DbContext
+    {
+        public MyRecipesContext()
+        {
+        }
+
+        public MyRecipesContext(DbContextOptions<MyRecipesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Color)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
